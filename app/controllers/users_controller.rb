5a4b1aa136ccebfadfd042f3e97c1d75f453dcf4
EC2 Default User <ec2-user@ip-172-31-95-126.ec2.internal>
2022-06-09 00:43:14 +0000

    7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = @user.books.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
     @book_new = Book.new
-    
+
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,25 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites,dependent: :destroy
+  has_many :favorited_users,through: :favorites,source: :user
   has_many :book_comments,dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
+  end
+
+
+  def self.search_for(content,method)
+    if method == "perfect"
+      Book.where(title: content)
+    elsif method == "forward"
+      Book.where("title LIKE ?",content+"%")
+    elsif method == "backward"
+      Book.where("title LIKE ?","%"+content )
+    else
+      Book.where("title LIKE ?","%"+content+"%")
+    end
   end
 end

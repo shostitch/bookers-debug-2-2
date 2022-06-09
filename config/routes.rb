38b33c7caa1e8504506b8f392851420c9f6ea4ba
EC2 Default User <ec2-user@ip-172-31-95-126.ec2.internal>
2022-06-09 10:22:25 +0000

    8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
   end
 
   get "search" => "searches#search"
+
+  resources :chats,only:[:create,:show]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
